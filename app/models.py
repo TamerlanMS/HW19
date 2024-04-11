cba8520ca350fa4daba9633775f5124efd281948
unknown <MusabekovTe@lcoast.itstep>
2024-04-11 21:09:23 +0500

    third commit

--- app/models.py
+++ app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class IcecreamShop(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+
+    def str(self):
+        return self.name
+
+class Icecream(models.Model):
+    flavor = models.CharField(max_length=100)
+    description = models.TextField()
+    shop = models.ForeignKey(IcecreamShop, on_delete=models.CASCADE, related_name='icecreams')
+
+    def str(self):
+        return self.flavor
